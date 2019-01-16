aba3329df9f1720ac80bad575df3f9314ff6a2a9
Igor Tarasuik <ihortarasyk@gmail.com>
2019-01-16 17:50:52 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+
+$('.parent-container').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  gallery:{
+  	enabled: true
+  }
+  // other options
+});
+
+});
+
+
